9bef2e288d583a169e472049df5cac3700efdb25
Agustin Cabra <cabra.agustin@gmail.com>
2020-09-19 01:45:13 +0200

    adding thymeleaf for static content templating

---
+++ src/main/java/com/acabra/testdiscoveryapp/web/SimpleController.java
@@ -0,0 +1,18 @@
+package com.acabra.testdiscoveryapp.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/hello")
+    public String getHomePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
